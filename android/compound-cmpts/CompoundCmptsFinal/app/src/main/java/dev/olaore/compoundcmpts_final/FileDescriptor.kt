074c0f411d6ff13e5707b0249d16a8a27b1337a4
Foodie <olaorefouad565@gmail.com>
2020-05-16 13:16:12 +0100

    continued working on part 2, set up the fileUri in the compound component, added the logic to get the image for the small thumbnail to the left, then we set up the filetype, looking good!

--- android/compound-cmpts/CompoundCmptsFinal/app/src/main/java/dev/olaore/compoundcmpts_final/FileDescriptor.kt
+++ android/compound-cmpts/CompoundCmptsFinal/app/src/main/java/dev/olaore/compoundcmpts_final/FileDescriptor.kt
@@ -1,13 +1,26 @@
 package dev.olaore.compoundcmpts_final
 
 import android.content.Context
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.file_descriptor.view.*
+import java.io.File
 
 class FileDescriptor @JvmOverloads
     constructor(private val ctx: Context, private val attributeSet: AttributeSet? = null, private val defStyleAttr: Int = 0)
     : ConstraintLayout(ctx, attributeSet, defStyleAttr) {
+
+    var fileUri: Uri? = null
+        set(value) {
+            field = value
+            setUpFileDescriptor()
+        }
+
+    var file: File? = null
+    var fileType: FileType? = null
 
     init {
 
@@ -22,4 +35,75 @@ class FileDescriptor @JvmOverloads
 
     }
 
+    private fun setUpFileDescriptor() {
+        setFile()
+        setUpFileType()
+    }
+
+    private fun setUpFileTypeImage() {
+        file_type_image.setImageResource(when(fileType) {
+            FileType.DOCX -> R.drawable.docx
+            FileType.IMAGE -> R.drawable.image
+            FileType.MP3 -> R.drawable.mp3
+            FileType.MP4 -> R.drawable.video
+            FileType.PDF -> R.drawable.pdf
+            FileType.TEXT -> R.drawable.txt
+            FileType.UNKNOWN -> R.drawable.no_file_selected
+            else -> R.drawable.no_file_selected
+        })
+    }
+
+    private fun setUpFileType() {
+        val resolver = ctx.contentResolver
+        val type = resolver.getType(fileUri!!)!!
+
+        fileType = if (type.contains("image")) {
+            FileType.IMAGE
+        } else if (type.contains("video")) {
+            FileType.MP4
+        } else if (type.contains("application/msword") || type.contains("application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
+            FileType.DOCX
+        } else if (type.contains("audio")) {
+            FileType.MP3
+        } else if (type.contains("application/pdf")) {
+            FileType.PDF
+        } else if (type.contains("text/plain")) {
+            FileType.TEXT
+        } else {
+            FileType.UNKNOWN
+        }
+
+        setUpFileTypeImage()
+        }
+    }
+
+    private fun setFile() {
+        // columns to be retrieved from the media content provider
+        val columns = arrayOf(MediaStore.Images.Media.DATA)
+        // query the content resolver for the data associated with the fileUri passed in
+        val cursor = ctx.contentResolver.query(
+            fileUri!!, columns, null, null, null
+        )
+
+
+        cursor?.let {
+            // move to the first item
+            it.moveToFirst()
+            // get the index for the data column on the content provider
+            val dataColumnIndex = it.getColumnIndex(columns[0])
+            // get the string value at the index
+            val filePath = cursor.getString(dataColumnIndex)
+            // close the cursor
+            it.close()
+            filePath?.let { path ->
+                file = File(path)
+            }
+        }
+
+    }
+
+}
+
+enum class FileType {
+    IMAGE, TEXT, PDF, DOCX, MP4, MP3, UNKNOWN
 }
--- android/compound-cmpts/CompoundCmptsFinal/app/src/main/java/dev/olaore/compoundcmpts_final/MainActivity.kt
+++ android/compound-cmpts/CompoundCmptsFinal/app/src/main/java/dev/olaore/compoundcmpts_final/MainActivity.kt
@@ -1,12 +1,38 @@
 package dev.olaore.compoundcmpts_final
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_FILE_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        select_file_button.setOnClickListener {
+            selectFile()
+        }
+
     }
+
+    private fun selectFile() {
+        val selectFileIntent = Intent(Intent.ACTION_GET_CONTENT)
+        selectFileIntent.type = "*/*"
+        startActivityForResult(selectFileIntent, REQUEST_FILE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_FILE_CODE && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                val fileUri = data.data
+            }
+        }
+    }
+
 }
