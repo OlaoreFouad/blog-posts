e69b93cee64ee93dabaf67f9bd24b2dda2626166
Foodie <olaorefouad565@gmail.com>
2020-07-28 20:19:29 +0100

    feature: added the design for bold, strikethrough and underlined text in a single text, sleeeekkk

--- android/spannables-demo/app/src/main/java/dev/olaore/spannablesdemo/MainActivity.kt
+++ android/spannables-demo/app/src/main/java/dev/olaore/spannablesdemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.text.style.*
 import android.widget.TextView
 
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var foregroundBackgroundText: TextView
     private lateinit var sizingText: TextView
     private lateinit var sizingBiggerText: TextView
+    private lateinit var boldStrikethroughUnderlineText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +28,49 @@ class MainActivity : AppCompatActivity() {
         foregroundBackgroundText = findViewById(R.id.foreground_and_background_text)
         sizingText = findViewById(R.id.sizing_text)
         sizingBiggerText = findViewById(R.id.sizing_bigger_text)
+        boldStrikethroughUnderlineText = findViewById(R.id.bold_strikethrough_underline_text)
 
         setBoldText()
         setForegroundText()
         setForegroundAndBackground()
         setSizingText()
         setSizingBiggerText()
+        setBoldStrikeThroughUnderlineText()
+
+    }
+
+    private fun setBoldStrikeThroughUnderlineText() {
+        val boldText = "This text is bold"
+        val boldStrUnderlineSpannable = SpannableStringBuilder(boldText)
+
+        val boldStartIndex = boldText.indexOf("bold")
+        val amountOfCharactersBold = "bold".length
+
+        boldStrUnderlineSpannable.setSpan(
+            StyleSpan(Typeface.BOLD), boldStartIndex, boldStartIndex + amountOfCharactersBold, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val strikethroughText = " and this is a strikethrough"
+        boldStrUnderlineSpannable.append(strikethroughText)
+
+        val strikethroughStartIndex = boldStrUnderlineSpannable.indexOf("strikethrough")
+        val amountOfCharacterstrikethrough = "strikethrough".length
+
+        boldStrUnderlineSpannable.setSpan(
+            StrikethroughSpan(), strikethroughStartIndex, strikethroughStartIndex + amountOfCharacterstrikethrough, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val underlineText = " and this is an underlined text"
+        boldStrUnderlineSpannable.append(underlineText)
+
+        val underlineStartIndex = boldStrUnderlineSpannable.indexOf("underlined")
+        val amountOfCharacterUnderline = "underlined".length
+
+        boldStrUnderlineSpannable.setSpan(
+            UnderlineSpan(), underlineStartIndex, underlineStartIndex + amountOfCharacterUnderline, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        boldStrikethroughUnderlineText.text = boldStrUnderlineSpannable
 
     }
 
