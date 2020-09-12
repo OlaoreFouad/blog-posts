94db07bebed54b3faafebab0e2b1ad22b1b3df8c
Foodie <olaorefouad565@gmail.com>
2020-09-12 17:10:18 +0100

    worked on the project for intro to custom views article, drew rects and circles, drew their outlined versions also using canvas translates, saves and restores. A dynamic layer was also added allowing a toggle button reset the colors of the shapes and then re-draw

---
+++ android/custom-views/app/src/main/java/dev/olaore/customviewsintro/MyFirstCustomView.kt
@@ -0,0 +1,105 @@
+package dev.olaore.customviewsintro
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import kotlin.random.Random
+
+class MyFirstCustomView @JvmOverloads
+    constructor(private val ctx: Context, private val attributeSet: AttributeSet? = null, private val defStyleAttr: Int = 0)
+    : View(ctx, attributeSet, defStyleAttr) {
+
+    val colors = listOf<Int>(Color.RED, Color.YELLOW, Color.DKGRAY, Color.BLACK, Color.MAGENTA)
+
+//    create the paint object for the rectangle
+    private val rectPaint = Paint()
+//    create the paint object for the circle
+    private val circlePaint = Paint()
+
+    //    create the outlined paint object for the rectangle
+    private val outlinedRectPaint = Paint()
+    //    create the outlined paint object for the circle
+    private val outlinedCirclePaint = Paint()
+
+    init {
+//        set the color to blue
+        rectPaint.color = Color.BLUE
+//        smoothen the edges using antiAlias flag
+        rectPaint.isAntiAlias = true
+
+//        set the color to red
+        circlePaint.color = Color.RED
+//        smoothen the edges using antiAlias flag
+        circlePaint.isAntiAlias = true
+
+        outlinedRectPaint.color = Color.GREEN
+        outlinedRectPaint.style = Paint.Style.STROKE
+        outlinedRectPaint.strokeWidth = 4f
+
+        outlinedCirclePaint.style = Paint.Style.STROKE
+        outlinedCirclePaint.strokeWidth = 4f
+        outlinedCirclePaint.color = Color.BLACK
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        drawRectangle(canvas)
+        drawCircle(canvas)
+
+//        make outlined draw calls
+        drawOutlinedRectangle(canvas)
+        drawOutlinedCircle(canvas)
+    }
+
+    private fun getRandomIndex(): Int {
+        return Random.nextInt(0, colors.size - 1)
+    }
+
+    private fun drawRectangle(canvas: Canvas?) {
+        canvas?.drawRect(0f, 0f, 150f, 150f, rectPaint)
+    }
+
+    private fun drawCircle(canvas: Canvas?) {
+        canvas?.drawCircle(
+            (width - 75f), 75f, 75f, circlePaint
+        )
+    }
+
+    private fun drawOutlinedRectangle(canvas: Canvas?) {
+
+        canvas?.save()
+
+//        translate the canvas
+        canvas?.translate(0f, 160f)
+
+        canvas?.drawRect(0f, 0f, 150f, 150f, outlinedRectPaint)
+        canvas?.restore()
+    }
+
+    private fun drawOutlinedCircle(canvas: Canvas?) {
+
+        canvas?.save()
+
+//        translate the canvas
+        canvas?.translate((width - 75f), 160f + 75f)
+
+        canvas?.drawCircle(
+            0f, 0f, 75f, outlinedCirclePaint
+        )
+    }
+
+    fun toggle() {
+        rectPaint.color = colors[getRandomIndex()]
+        circlePaint.color = colors[getRandomIndex()]
+        outlinedRectPaint.color = colors[getRandomIndex()]
+        outlinedCirclePaint.color = colors[getRandomIndex()]
+
+        invalidate()
+    }
+
+}
+
