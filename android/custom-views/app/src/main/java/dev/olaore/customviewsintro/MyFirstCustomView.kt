438da24d8f6ee047dfb4740183f2b2fa0fed0717
Foodie <olaorefouad565@gmail.com>
2020-10-05 21:10:08 +0100

    feature: made changes for intro to custom views || article

--- android/custom-views/app/src/main/java/dev/olaore/customviewsintro/MyFirstCustomView.kt
+++ android/custom-views/app/src/main/java/dev/olaore/customviewsintro/MyFirstCustomView.kt
@@ -12,7 +12,7 @@ class MyFirstCustomView @JvmOverloads
     constructor(private val ctx: Context, private val attributeSet: AttributeSet? = null, private val defStyleAttr: Int = 0)
     : View(ctx, attributeSet, defStyleAttr) {
 
-    val colors = listOf<Int>(Color.RED, Color.YELLOW, Color.DKGRAY, Color.BLACK, Color.MAGENTA)
+    private val colors = listOf<Int>(Color.RED, Color.YELLOW, Color.DKGRAY, Color.BLACK, Color.MAGENTA)
 
 //    create the paint object for the rectangle
     private val rectPaint = Paint()
@@ -35,8 +35,11 @@ class MyFirstCustomView @JvmOverloads
 //        smoothen the edges using antiAlias flag
         circlePaint.isAntiAlias = true
 
+//        set the color of the paint object
         outlinedRectPaint.color = Color.GREEN
+//        set the style of paint to be used
         outlinedRectPaint.style = Paint.Style.STROKE
+//        set the width of the stroke
         outlinedRectPaint.strokeWidth = 4f
 
         outlinedCirclePaint.style = Paint.Style.STROKE
@@ -71,21 +74,17 @@ class MyFirstCustomView @JvmOverloads
 
     private fun drawOutlinedRectangle(canvas: Canvas?) {
 
-        canvas?.save()
-
 //        translate the canvas
         canvas?.translate(0f, 160f)
 
+//        draw rectangle
         canvas?.drawRect(0f, 0f, 150f, 150f, outlinedRectPaint)
-        canvas?.restore()
     }
 
     private fun drawOutlinedCircle(canvas: Canvas?) {
 
-        canvas?.save()
-
 //        translate the canvas
-        canvas?.translate((width - 75f), 160f + 75f)
+        canvas?.translate((width - 75f), 75f)
 
         canvas?.drawCircle(
             0f, 0f, 75f, outlinedCirclePaint
