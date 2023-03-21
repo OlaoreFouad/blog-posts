79654b6535f7d8c7957da6966d43df4bde5df375
Olaore Fouad <olaorefouad565@gmail.com>
2023-03-21 11:01:44 +0100

    feat: added layout for formatting flow on custom views article

---
+++ android/WorkingWithText/app/src/main/java/dev/olaore/workingwithtext/FormattingActivity.kt
@@ -0,0 +1,11 @@
+package dev.olaore.workingwithtext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormattingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formatting)
+    }
+}
