bf3ac12d4e9f8bd8b1e22cffa4ffbc80ff00cf18
Olaore Fouad <olaorefouad565@gmail.com>
2023-03-21 11:07:42 +0100

    fix: fixed layout

--- android/WorkingWithText/app/src/main/java/dev/olaore/workingwithtext/FormattingActivity.kt
+++ android/WorkingWithText/app/src/main/java/dev/olaore/workingwithtext/FormattingActivity.kt
@@ -2,10 +2,18 @@ package dev.olaore.workingwithtext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.olaore.workingwithtext.views.CustomTextView
 
 class FormattingActivity : AppCompatActivity() {
+
+    private lateinit var customTextView: CustomTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formatting)
+
+        customTextView = findViewById(R.id.custom_text_view)
+
+        customTextView.setText("Default Text")
     }
 }
